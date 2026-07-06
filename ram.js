2676435df20de5672171eff0fbcea8d83b58d18f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2026-07-06 20:42:40 +0530

    tutorial

---
+++ ram.js
@@ -0,0 +1,68 @@
+//multiple store data type
+// let person = {
+//     name: "John",
+//     age: 30
+// };
+
+// console.log(person.name , person.age);
+
+//type of 
+// const Sai=["sam","ram","saasa"]
+
+// console.log(typeof Sai[0]);
+
+//Arithmetic operator
+
+// let a=10;
+// let b=20;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+//greater small and equal operator
+// let a = 32;
+// let b = 90;
+// console.log(a<b);
+
+
+//logical operator
+//&& both true and || one true 
+// let sam=89;
+
+// let ram=90;
+
+// if(sam<=21 || ram>=21){
+//     console.log("eligible for vote");
+    
+// }
+// else{
+//     console.log("not eligible for vote");
+// }
+
+//if else statement
+
+// const sana=50;
+
+// if(sana>80){
+// console.log("sana is greater than 90")
+
+// }
+// else {
+//     console.log("sana is less than 90")
+// }
+//else if statement
+
+// let marks = 78;
+
+// if (marks >= 90) {
+//     console.log("A");
+// } else if (marks >= 75) {
+//     console.log("B");
+// } else if (marks >= 60) {
+//     console.log("C");
+// } else {
+//     console.log("Fail");
+// }
+
